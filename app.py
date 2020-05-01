8dc7f3800709eab1138ec31fdce07a31e769f555
noah.Nam <noah85.Nam@gmail.com>
2020-05-02 01:49:48 +0900

    miniter init

---
+++ app.py
@@ -0,0 +1,131 @@
+from flask import Flask, request, jsonify
+from flask.json import JSONEncoder
+
+'''
+Default JSON encoder는 set을 JSON으로 변환할 수 없다.
+그러므로 커스텀 엔코더를 작성해서 set을 list로 변환하여 JSON으로 변환가능하게 해주어야 한다.
+'''
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+
+        if isinstance(obj, set):
+            return list(obj)
+
+        return JSONEncoder.default(self, obj)
+
+
+app = Flask(__name__)
+
+app.users = {}
+app.tweets = []
+app.id_count = 1
+app.json_encoder = CustomJSONEncoder
+
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "pong"
+
+
+@app.route('/sign-up', methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user['id'] = app.id_count
+
+    app.users[app.id_count] = new_user
+    app.id_count += 1
+
+    return jsonify(app.users)
+
+
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return jsonify({
+            'msg': '사용자가 존재하지 않습니다',
+            'status_code': 400
+        })
+
+    if len(tweet) > 300:
+        return jsonify({
+            'msg': '300자를 초과했습니',
+            'status_code': 400
+        })
+
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+
+    return jsonify({
+        'msg': '트윗이 성공했습니다',
+        'status_code': 200
+        })
+
+
+@app.route('/timeline/<int:user_id>', methods=['GET'])
+def timeline(user_id):
+    if user_id not in app.users:
+        return jsonify({
+            'msg': '사용자가 존재하지 않습니다',
+            'status_code': 400
+        })
+
+    # follow_list = app.users[user_id].get('follow')
+    follow_list = app.users[user_id].get('follow', set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify({
+        'user_id': user_id,
+        'timeline': timeline
+    })
+
+
+@app.route('/follow', methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return jsonify({
+            'msg': '사용자가 존재하지 않습니다',
+            'status_code': 400
+        })
+
+    user = app.users[user_id]
+    print('-->', app.users)
+    print('-->', user)
+    print('-->', type(user))
+    user.setdefault('follow', set()).add(user_id_to_follow)
+
+    return jsonify(user)
+
+
+@app.route('/unfollow', methods=['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['unfollow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return jsonify({
+            'msg': '사용자가 존재하지 않습니다',
+            'status_code': 400
+        })
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).discard(user_id_to_follow)
+
+    return jsonify(user)
+
+
+if __name__ == '__main__':
+    app.run()
